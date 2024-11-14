1980b8ddad08c3bc4e899401697fad068d66f0ea
tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-11-14 08:51:41 +0100

    Punto 5

--- listaspesa.py
+++ listaspesa.py
@@ -14,3 +14,20 @@ def eliminaElemento():
     risp.lower()
     lista.remove(risp)
 
+controllo = True
+while controllo == True:
+    print("1) Aggiungi un elemento alla lista")
+    print("2) Visualizza la lista")
+    print("3) Elimina un elemento della lista")
+    x = input ("Inserisci il numero di ciò che vuoi fare ")
+    if x == 1:
+        aggiunta()
+    elif x == 2:
+        stampaLista()
+    elif x == 3:
+        eliminaElemento()
+    else:
+        uscita = input("Vuoi uscire dal programma? (y/n)")
+        uscita.lower()
+        if uscita == "y":
+            controllo = False
