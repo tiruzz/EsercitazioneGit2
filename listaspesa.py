c102cfe94eaade070b1aef0f72cc21283ca5f350
tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-11-14 08:41:41 +0100

    Punto 4

--- listaspesa.py
+++ listaspesa.py
@@ -7,3 +7,10 @@ def aggiunta():
 
 def stampaLista():
     print(lista)
+
+def eliminaElemento():
+    print(lista)
+    risp = input ("inserisci il nome dell'elemento che vuoi eliminare")
+    risp.lower()
+    lista.remove(risp)
+
