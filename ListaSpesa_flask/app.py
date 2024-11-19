23b636e997366c3928e976bea9a0ab2cfe7e05c4
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-11-19 08:44:20 +0000

    dio cerca di essere clemente con me gruzie

--- ListaSpesa_flask/app.py
+++ ListaSpesa_flask/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #inizializza l'app Flask
 app = Flask(__name__)
 
+listaSpesa=["mamma"]
+
 #rotta principale
 @app.route('/')
+def home():
+    return render_template('index.html')
 
 def home():
     return "Per ora funziona tutto"
